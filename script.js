8b9cb3b08df780e639dadfdd6ce2e9995777b127
Eric <ericnguyen23@gmail.com>
2023-02-22 10:08:50 -0800

    Added yes or no to a variable

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 // Storing variables and setting to false and 0 initially
-var allowuUppercase = false;
+var allowUppercase = false;
 var allowNumbers = false;
 var allowCharacters = false;
 var pwLength = 0;
 
 // Prompt function
 function prompts() {
+  var okyes = "OK for yes, cancel for no.";
   // using confirm method to set variables to boolean based on response
-  allowUppercase = confirm("Would you like to include Uppercase characters?");
-  allowNumbers = confirm("Would you like to include numbers?");
-  allowCharacters = confirm("Would you like to include special characters?");
-  pwLength = prompt(
-    "What length would you like your password to be? Please at least 8 characters and at most 128"
+  allowUppercase = confirm(
+    "Would you like to include Uppercase characters? " + okyes
   );
+  allowNumbers = confirm("Would you like to include numbers? " + okyes);
+  allowCharacters = confirm(
+    "Would you like to include special characters? " + okyes
+  );
+  pwLength = prompt(
+    "What length would you like your password to be? Please at least 8 characters and at most 128."
+  );
+
+  // validate if user has entered a value for pw length
+  // if (pwLength == "") {
+  //   pwLength = prompt(
+  //     "PLEASE ENTER A VALUE. What length would you like your password to be? Please at least 8 characters and at most 128."
+  //   );
+  // }
+
+  // validate if user entered a numerical value for pw lentgh
 }
 
 // Generate password
@@ -25,7 +39,7 @@ function generatePassword(length) {
   var userAcceptedChars = "abcdefghijklmnopqrstuvwxyz";
   var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var nums = "0123456789";
-  var specialChars = "!@#$%^&*()./-=_+~`|}{[]:;?><,";
+  var specialChars = "!@#$%^&*()./-=_+~`|}{[]:;?><";
 
   // Check if user allows certain criteria and add to userAcceptedChars var
   if (allowUppercase) {
@@ -40,11 +54,15 @@ function generatePassword(length) {
     userAcceptedChars += specialChars;
   }
 
+  // Mix up userAcceptedChars at random first
+
   // Loop to add to password depending on length provided by user
   for (var i = 0; i < length; i++) {
     result += userAcceptedChars.charAt(Math.floor(Math.random() * length));
     counter += 1;
   }
+
+  console.log(userAcceptedChars);
 
   return result;
 }
